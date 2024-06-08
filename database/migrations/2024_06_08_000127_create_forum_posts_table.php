bf3dc8c69da9e807e63d8fbe55f85a22de4cad69
chimfwembeC <chimfwembek@kamstar.tech>
2024-06-08 02:06:08 +0200

    migrations

---
+++ database/migrations/2024_06_08_000127_create_forum_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forum_posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('forum_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('content');
+            $table->foreign('forum_id')->references('id')->on('forums')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forum_posts');
+    }
+};
